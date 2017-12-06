334d95fe86ab0bfa11277d81b38caf8b5145a873
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-06 12:43:27 -0700

    step 5-6

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const books = [];
+let id = 0;
+
+app.post('/api/books',(req, res) => {
+    console.log('You sent me this data: ', req.body);
+    books.push({
+        id: id,
+        title: req.body.title,
+        author: req.body.author
+    });
+    id++
+    console.log('books',books);
+    res.json(books);
+});
+
 app.listen(3000, () => {
     console.log('Server listening on port ' + 3000);
 });
