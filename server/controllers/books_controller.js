1348a580b8537bcfd0286b5ec64ac54298cad7ad
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-06 14:44:53 -0700

    step 6 continue

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -1,25 +1,31 @@
+import { read } from "fs";
+
 const books = [];
 let id = 0;
 
-app.post('/api/books',(req, res) => {
-    console.log('You sent me this data: ', req.body);
-    books.push({
-        id: id,
-        title: req.body.title,
-        author: req.body.author
-    });
-    id++
-    console.log('books',books);
-    res.json(books);
-});
+module.exports = {
+    create(req, res) {
+        console.log('You sent me this data: ', req.body);
+        books.push({
+            id: id,
+            title: req.body.title,
+            author: req.body.author
+        });
+        id++
+        console.log('books',books);
+        res.json(books);
+    }
+    // read
+    // update
+    delete(req, res) {
+        const bookId = req.params.id;
+        const bookIndex = books.findIndex(book => book.id === +bookId);
+        books.splice(bookIndex,1);
+        res.json(books);
+    }
+};
 
-app.get('/api/books');
 
-app.put('/api/books/:id');
+// app.get('/api/books');
 
-app.delete('/api/books/:id', (req, res) => {
-    const bookId = req.params.id;
-    const bookIndex = books.findIndex(book => book.id === +bookId);
-    books.splice(bookIndex,1);
-    res.json(books);
-});
+// app.put('/api/books/:id');
--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bc = require('./controllers/books_controller');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+app.post('/api/books', bc.create);
+// app.get('/api/books', bc.read);
+// app.put('/api/books/:id', bd.update);
+app.delete('/api/books/:id', bc.delete);
 
 
 app.listen(3000, () => {
